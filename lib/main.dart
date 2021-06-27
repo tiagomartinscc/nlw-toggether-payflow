8ec5c2a0ab2087bc8b71ff70c5a474de9ecd464b
Tiago Martins <tiagomartinscc@hotmail.com>
2021-06-27 08:33:15 -0300

    aulas 1 e 2

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/modules/home/home_page.dart';
+import 'package:payflow/modules/login/login_page.dart';
+import 'package:payflow/modules/splash/spash_page.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+
+void main() {
+  runApp(AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pay flow',
+      theme: ThemeData(
+        primaryColor: AppColors.primary,
+      ),
+      home: HomePage(),
+    );
+  }
+}
